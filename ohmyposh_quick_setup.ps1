820e7dfdb308cd617230b4bebc3a8f75af74cce4
ari-w <ariweinberg326@gmail.com>
2023-07-13 15:30:16 -0400

    use winget instaed of manual install

--- ohmyposh_quick_setup.ps1
+++ ohmyposh_quick_setup.ps1
@@ -1,4 +1,6 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+# Set-ExecutionPolicy Bypass -Scope Process -Force
+# Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+
+winget install JanDeDobbeleer.OhMyPosh -s winget
 
 Invoke-WebRequest -URI 'https://github.com/Ari-Weinberg/ohmyposh_quick_setup/raw/main/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf' -OutFile 'c:\windows\temp\jetbrains_mono.ttf'
