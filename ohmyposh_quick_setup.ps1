dc6f0959bfe2ad04f883bd4b76679303e3190f36
ari-w <ariweinberg326@gmail.com>
2023-07-13 15:08:59 -0400

    Add font download

--- ohmyposh_quick_setup.ps1
+++ ohmyposh_quick_setup.ps1
@@ -1,3 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 
+Invoke-WebRequest -URI 'https://github.com/Ari-Weinberg/ohmyposh_quick_setup/raw/main/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf' -OutFile 'c:\windows\temp\jetbrains_mono.ttf'
