d5a1f1cf1778a5f3f345d635e65afef8de2f6c72
ari-w <ariweinberg326@gmail.com>
2023-07-13 15:43:09 -0400

    add font install

--- ohmyposh_quick_setup.ps1
+++ ohmyposh_quick_setup.ps1
@@ -4,3 +4,36 @@
 winget install JanDeDobbeleer.OhMyPosh -s winget
 
 Invoke-WebRequest -URI 'https://github.com/Ari-Weinberg/ohmyposh_quick_setup/raw/main/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf' -OutFile 'c:\windows\temp\jetbrains_mono.ttf'
+
+Install-Font -FontFilePath "c:\windows\temp\jetbrains_mono.ttf"
+
+function Install-Font {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$FontFilePath
+    )
+
+    # Get the font name from the file path
+    $FontName = [System.IO.Path]::GetFileNameWithoutExtension($FontFilePath)
+
+    # Check if the font is already installed
+    $fontKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts"
+    $fontRegistry = Get-ItemProperty -Path $fontKeyPath
+    if ($fontRegistry -and $fontRegistry."$FontName (TrueType)" -eq $FontFilePath) {
+        Write-Host "Font '$FontName' is already installed."
+        return
+    }
+
+    # Install the font
+    Write-Host "Installing font '$FontName'..."
+
+    # Copy the font file to the Windows fonts folder
+    $fontsFolderPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('Fonts'), "$FontName.ttf")
+    Copy-Item -Path $FontFilePath -Destination $fontsFolderPath -Force
+
+    # Add the font to the registry
+    $fontNameKey = "$FontName (TrueType)"
+    Set-ItemProperty -Path $fontKeyPath -Name $fontNameKey -Value $fontsFolderPath
+
+    Write-Host "Font '$FontName' has been installed."
+}
